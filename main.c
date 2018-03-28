e0d620704c6f813006bbb831e4fabe53c7e999d5
flo <florian.harold@hotmail.com>
2018-03-28 20:25:05 +0200

    Added -name and -user action the addition from -ls  is in progress action new parameter to resolve path action=1 => -name action=3 => -user action=6 => -ls Hope you unterstand my changes...

--- main.c
+++ main.c
@@ -9,13 +9,15 @@
 #include <string.h>    /* for strerror() */
 #include <memory.h>     /* for strerror() */
 #include <sys/stat.h>
+#include<fnmatch.h>
+#include<pwd.h>
 
 enum Action {notdeclared, noaction, user, name, type, print, ls, nouser, path};
 
 
-DIR *do_file (DIR *pDIR,  char *pPATH);
-DIR *do_dir ( char *pPATH/*, char *pFULLpath*/);
-int resolve_relpath(char* pPATH,int count);
+DIR *do_file (DIR *pDIR,  char *pPATH,int action, char* arg,int output);
+DIR *do_dir ( char *pPATH, int action,char* arg);
+int resolve_relpath(char* pPATH,int count,int action,char* arg);
 char *func_check_path(char* pPATH);
 enum Action func_check_action(char *pACTION);
 void func_check_arguments(char *pARGUMENTS[], int COUNTER, enum Action *pArrAction, char *pArrArgument[]);
@@ -25,12 +27,13 @@ int main(int argc, char *argv[]){
     char *pMainPath = NULL;     //Path for the find functions
     enum Action pArrMainAction[argc];
     char *pArrMainArgument[argc];
+    int action=6;
 
     func_check_arguments(argv,argc,pArrMainAction,pArrMainArgument);
 
     pMainPath = func_check_path(argv[1]);
 
-    resolve_relpath(pMainPath,argc);
+    resolve_relpath(pMainPath,argc,action,argv[3]);
 
      return 0;
 }
@@ -138,80 +141,122 @@ char *func_print_action(enum Action eAction){
     }
 }
 
-DIR *do_file (DIR *pDIR, char* pPATH){
+DIR *do_file (DIR *pDIR, char* pPATH, int action, char* arg,int output){
 
 
     //declaration of the variables
     struct stat file;
+    struct passwd *user=NULL;
 
+        if (lstat(pPATH, &file) == -1) {
+            printf("ERROR");
+        }
 
-    if(lstat(pPATH,&file)==-1){
-        printf("ERROR");
+        if (S_ISREG(file.st_mode)) {
+            if((output==1&&action==1)||(action==7)||(action==0)) {
+                printf("\nFile%s", pPATH);
+            }else if(action==3) {
+                if((user=getpwnam(arg))==NULL){         //save user information in struct passwd user
+                    printf("ERROR");
+                } else {
+                    if(file.st_uid==user->pw_uid){
+                        printf("\nUFile%s", pPATH);
+                    }
+                }
 
-    }else if(S_ISREG(file.st_mode)){
+            }else if(action==6){
+                printf("%d",file.st_mode);
+                printf("-");
+                printf( (file.st_mode & S_IRUSR) ? "r" : "-");
+                printf( (file.st_mode & S_IWUSR) ? "w" : "-");
+                printf( (file.st_mode & S_IXUSR) ? "x" : "-");
+                printf( (file.st_mode & S_IRGRP) ? "r" : "-");
+                printf( (file.st_mode & S_IWGRP) ? "w" : "-");
+                printf( (file.st_mode & S_IXGRP) ? "x" : "-");
+                printf( (file.st_mode & S_IROTH) ? "r" : "-");
+                printf( (file.st_mode & S_IWOTH) ? "w" : "-");
+                printf( (file.st_mode & S_IXOTH) ? "x" : "-");
 
-        printf("\nFile%s",pPATH);
+            }
 
+        } else if (S_ISDIR(file.st_mode)) {
+            if((output==1&&action==1)||(action==7)||(action==0)) {
+                printf("\nDirectory:%s", pPATH);
+            }else if(action==3) {
 
-    }else if(S_ISDIR(file.st_mode)){
+                if((user=getpwnam(arg))==NULL){
+                    printf("ERROR");
+                } else {
+                    if(file.st_uid==user->pw_uid){
+                        printf("\nUDirectory:%s", pPATH);
+                    }
+                }
+            }
 
-        printf("\nDirectory:%s",pPATH);
-        do_dir(pPATH);
-
-    }
+            do_dir(pPATH, action, arg);
+        }
 
 
 }
 
 
-DIR *do_dir ( char *pPATH/*, char *pFULLpath*/){
+DIR *do_dir ( char *pPATH, int action,char* arg) {
     //open the directory if the name has an other name as "." or ".."
     //relative path: ../name1/name2/name3
     DIR *pDIR = NULL;
     struct dirent *pdirent = NULL;
     int length;
+    int output = 0;
 
-    pDIR=opendir(pPATH);
 
-    if ( pDIR == NULL) {
+    pDIR = opendir(pPATH);
+
+    if (pDIR == NULL) {
 
         printf("Error");
         printf(" %s", strerror(errno)); //print the error message
 
     } else {
 
-        while((pdirent=readdir(pDIR))!=NULL){
+        while ((pdirent = readdir(pDIR)) != NULL) {
 
-            if(strcmp(pdirent->d_name,".")==0 || strcmp(pdirent->d_name,"..")==0){
-                    continue;
+            if (strcmp(pdirent->d_name, ".") == 0 || strcmp(pdirent->d_name, "..") == 0) {
+                continue;
             }
 
-            length=strlen(pPATH)+ 2  + strlen(pdirent->d_name);
+            length = strlen(pPATH) + 2 + strlen(pdirent->d_name);
             char newpath[length];
-            strcpy(newpath,"\0");
-            strcat(newpath,pPATH);
-            strcat(newpath,"/");
-            strcat(newpath,pdirent->d_name);
-            strcat(newpath,"\0");
-            do_file(pDIR,newpath);
+            strcpy(newpath, "\0");
+            strcat(newpath, pPATH);
+            strcat(newpath, "/");
+            strcat(newpath, pdirent->d_name);
+            strcat(newpath, "\0");
+                                                     //-name
+                    if (!(fnmatch(arg, pdirent->d_name, FNM_PATHNAME))) {
+                        output = 1;
+                    }
 
+
+
+            do_file(pDIR, newpath, action, arg, output);
+            output=0;
         }
-        closedir(pDIR);
-    }
 
+        closedir(pDIR);
+        }
 
     return 0;
 }
 
 
-int resolve_relpath(char* pPATH,int count){
+int resolve_relpath(char* pPATH,int count,int action,char* arg){
 
     int i=0;
 
 
     if((count==1)||((count>1)&&((strcmp(pPATH,"~")==0)||(strcmp(pPATH,".")==0)))){
 
-        do_dir(".");
+        do_dir(".",action,arg);
         //free(pPATH);
      // }
 
@@ -234,17 +279,17 @@ int resolve_relpath(char* pPATH,int count){
 
             pPATH[i]='\0';
 
-            do_dir(pPATH);
+            do_dir(pPATH,action,arg);
 
             free(pPATH);
         }
 
     }else if(count>1 && (pPATH[0]=='/')){
 
-        do_dir(pPATH);
+        do_dir(pPATH,action,arg);
 
     }else if(count>1 && (pPATH[0]!='/')){
 
-        do_dir(pPATH);
+        do_dir(pPATH,action,arg);
     }
 }
